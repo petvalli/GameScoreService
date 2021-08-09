d0a9c782a1c68c3799ed51de95387b36580fa64d
miluukka <miluukka@paju.oulu.fi>
2021-08-09 18:02:23 +0300

    added view funcs

--- gamescoreservice/api.py
+++ gamescoreservice/api.py
@@ -4,10 +4,17 @@ from flask_restful import Api
 api_bp = Blueprint("api", __name__, url_prefix="/api")
 api = Api(api_bp)
 
+import json
 from gamescoreservice.resources.player import PlayerCollection, PlayerItem, ScoresByCollection
 from gamescoreservice.resources.score import ScoreItem
 from gamescoreservice.resources.game import GameCollection, GameItem
 from gamescoreservice.resources.level import LevelItem
+from gamescoreservice.constants import *
+from gamescoreservice.utils import ScoreBuilder
+from flask import Response, redirect
+
+
+# Resources:
 
 api.add_resource(PlayerCollection, "/players/")
 api.add_resource(PlayerItem, "/players/<player>/")
@@ -17,6 +24,35 @@ api.add_resource(GameItem, "/games/<game>/")
 api.add_resource(LevelItem, "/games/<game>/<level>/")
 api.add_resource(ScoreItem, "/games/<game>/<level>/<player>/")
 
-#@api.route("/" + API_NAME + "/link-relations/")
-#def redirect_to_apiary_link_rels():
-#    return redirect(APIARY_URL + "link-relations")
+
+# View functions:
+
+@api_bp.route("/")
+def entry_point():
+    """
+    Entry point has controls to go to list of all games or all players.
+    """
+
+    body = ScoreBuilder()
+    body.add_namespace("gss", LINK_RELATIONS_URL)
+    body.add_control_games_all()
+    body.add_control_players_all()
+    return Response(json.dumps(body), 200, mimetype=MASON)
+
+
+@api_bp.route(LINK_RELATIONS_URL)
+def redirect_link_relations():
+    """
+    Redirect link relations to the Apiary documentation.
+    """
+
+    return redirect(APIARY_URL + "link-relations")
+
+
+@api_bp.route("/profiles/<profile>/")
+def redirect_profiles(profile):
+    """
+    Redirect profiles to the Apiary documentation.
+    """
+
+    return redirect(APIARY_URL + "profiles")
